840a3bbc334d9540296215df2951ce400b720010
NR <nr>
2022-01-29 21:58:34 -0700

    fixes no links

--- lib/views/pages/coding/components/icon_text_button.dart
+++ lib/views/pages/coding/components/icon_text_button.dart
@@ -1,4 +1,4 @@
-// import 'dart:js' as js;
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
 
 class IconTextButton extends StatelessWidget {
@@ -54,39 +54,9 @@ class IconTextButton extends StatelessWidget {
         )
       ],
     );
-    return LayoutBuilder(builder: (context, constraints) {
-      print('${constraints.maxHeight},${constraints.maxWidth}');
-
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          InkWell(
-            onTap: onTap,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.asset(
-                assetPath,
-                height: 60,
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16),
-            child: Text(
-              label,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.subtitle1,
-              // overflow: TextOverflow.ellipsis,
-            ),
-          )
-        ],
-      );
-    });
   }
 }
 
 void launchURL(String url) async {
-  // js.context.callMethod('open', [url]);
+  js.context.callMethod('open', [url]);
 }
